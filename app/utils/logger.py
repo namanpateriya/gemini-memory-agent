5e2c106b2b2f5d066e1efbec2898660940c629ce
namanpateriya <pateriyanaman@gmail.com>
2026-05-09 12:53:18 +0530

    Create logger.py

---
+++ app/utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def get_logger(name):
+    if not logging.getLogger().hasHandlers():
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        )
+
+    return logging.getLogger(name)
